bf3e19fbd9f8db6752130df41a04ca4a9a396d5b
Navani <69040322+KNS786@users.noreply.github.com>
2020-12-23 22:20:14 +0530

    Update atlas.js

--- server/atlas.js
+++ server/atlas.js
@@ -1,5 +1,5 @@
 module.exports={
-   MONGOURI:'mongodb+srv://navaninayak:mlSqDll5j2QuMBxC@cluster0.41re4.mongodb.net/mockmovie?retryWrites=true&w=majority',
+   MONGOURI:'mongodb+srv://<ENTER NAME >:mlSqDll5j2QuMBxC@cluster0.41re4.mongodb.net/mockmovie?retryWrites=true&w=majority',
    JWT_SECRET:'WWtatstasrtDDwqqq'
 
 }
